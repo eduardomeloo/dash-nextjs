ad07eeff47e979bb26513524646eca991aa7aa17
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-01-21 11:44:05 -0300

    Ajuste de layout para 6 colunas no componente TopCards

--- components/TopCards.jsx
+++ components/TopCards.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TopCards = () => {
     return (
-        <div className='grid lg:grid-cols-5 gap-4 p-4'>
+        <div className='grid lg:grid-cols-6 gap-4 p-4'>
             <div className='lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg'>
                 <div className='flex flex-col w-full pb-4'> 
                     <p className='text-2xl font-bold'>R$9,84</p>
@@ -21,9 +21,9 @@ const TopCards = () => {
                     <span className='text-green-700 text-lg'>+10%</span>
                 </p>
             </div>
-            <div className='bg-white flex justify-between w-full border p-4 rounded-lg'>
+            <div className='lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg'>
                 <div className='flex flex-col w-full pb-4'> 
-                    <p className='text-2xl font-bold'>R$9,84</p>
+                    <p className='text-2xl font-bold'>R$158,84</p>
                     <p className='text-gray-600'>MGLU3</p>
                 </div>
                 <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg'>
