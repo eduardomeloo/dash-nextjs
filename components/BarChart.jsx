8e58cd4f259a141ab4ea20fe2197e2e42692a22e
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-01-21 12:35:33 -0300

    New components - BarChart and ReentOrders

---
+++ components/BarChart.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react'
+import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+const BarChart = () => {
+    const [chartData, setChartData] = useState({
+        datasets: []
+    })
+    const [chartOptions, setChartOptions] = useState({})
+
+    useEffect(() => {
+        setChartData({
+            labels: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'],
+            datasets: [
+                {
+                    label: 'Vendas R$',
+                    data: [15678, 65478, 94157, 12548, 84751, 55871, 98745],
+                    borderColor: 'rgb(53, 162, 235)',
+                    backgroundColor: 'rgb(53, 162, 235, 0.4)',
+                }
+            ]
+        })
+        setChartOptions({
+            plugins: {
+                legend: {
+                    position: 'top'
+                },
+                title: {
+                    display: true,
+                    text: 'Venda da Semana'
+                }
+            },
+            maintainAspectRatio: false,
+            responsive: true
+        })
+    }, [])
+    return (
+        <>
+            <div className='w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white'>
+                <Bar data={chartData} options={chartOptions} />
+            </div>
+        </>
+    )
+}
+
+export default BarChart
